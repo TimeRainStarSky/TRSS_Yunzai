be5aebedce631b63f13a780a7338ee211517949b
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-07-28 01:01:07 +0800

    修复 pacman 报错 文件系统中已存在

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202207170
+NAME=v1.0.0;VERSION=202207270
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -12,7 +12,7 @@ DIR="$HOME/TRSS_Yunzai"
 command -v pacman &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到 pacman 命令，请确认安装了正确的 Arch Linux 环境"
-pacman -Syu --noconfirm --needed curl git nano perl tmux||abort "依赖安装失败"
+pacman -Syu --noconfirm --needed --overwrite "*" curl git nano perl tmux||abort "依赖安装失败"
 abort_update(){ echo "
 $R! $@$O";[ "$N" -lt 8 ]&&{ let N++;download;}||abort "脚本下载失败，请检查网络，并尝试重新下载";}
 download(){ case "$N" in
