3611e7a6528bb2a2905335bc4994dddd8c256b96
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-08-22 01:15:00 +0800

    细节优化

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202208210
+NAME=v1.0.0;VERSION=202208211
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -16,8 +16,7 @@ pacman -Syu --noconfirm --needed --overwrite "*" curl git libnewt micro neofetch
 command -v locale-gen &>/dev/null&&{ echo "
 $Y- 正在设置语言$O
 "
-echo "zh_CN.UTF-8 UTF-8">/etc/locale.gen||abort "语言写入失败"
-locale-gen||abort "语言设置失败";}
+echo "zh_CN.UTF-8 UTF-8">/etc/locale.gen&&locale-gen||abort "语言设置失败";}
 abort_update(){ echo "
 $R! $@$O";[ "$N" -lt 10 ]&&{ let N++;download;}||abort "脚本下载失败，请检查网络，并尝试重新下载";}
 download(){ case "$N" in
