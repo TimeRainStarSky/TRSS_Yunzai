657a669fbc160d11b500f76715959da6b0e194e6
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-10-14 23:11:32 +0800

    新增 实时网速 nethogs

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202210120
+NAME=v1.0.0;VERSION=202210140
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -14,7 +14,7 @@ CMDPATH="${CMDPATH:-/usr/local/bin}"
 type pacman &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到 pacman 命令，请确认安装了正确的 Arch Linux 环境"
-pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl micro ranger neofetch htop ncdu||abort "依赖安装失败"
+pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl micro ranger neofetch htop nethogs ncdu||abort "依赖安装失败"
 type locale-gen &>/dev/null&&{ echo "
 $Y- 正在设置语言$O
 "
