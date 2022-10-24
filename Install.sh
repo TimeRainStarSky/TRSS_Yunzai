a21f1f21886288ab56517cda60f6c4f9f8f1b90a
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-10-24 08:13:57 +0800

    细节优化

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202210210
+NAME=v1.0.0;VERSION=202210240
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -18,7 +18,8 @@ pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl micro
 type locale-gen &>/dev/null&&{ echo "
 $Y- 正在设置语言$O
 "
-sed -i 's/#.*zh_CN\.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen&&locale-gen||abort "语言设置失败";}
+sed -i 's/#.*zh_CN\.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen&&locale-gen||echo "
+$R! 语言设置失败$O";}
 abort_update(){ echo "
 $R! $@$O";[ "$N" -lt 10 ]&&{ let N++;download;}||abort "脚本下载失败，请检查网络，并尝试重新下载";}
 download(){ case "$N" in
