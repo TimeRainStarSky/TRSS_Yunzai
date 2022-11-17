6b62ad0b07088e62983e68838e9d2fea3e78c14f
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-11-17 12:05:15 +0800

    细节优化

--- Install-MSYS2.sh
+++ Install-MSYS2.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai MSYS2 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202211175
+NAME=v1.0.0;VERSION=202211176
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -16,7 +16,7 @@ MSYS2ENV=mingw-w64-ucrt-x86_64
 type pacman &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到 pacman 命令，请确认安装了正确的 MSYS2 环境"
-pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl fish ncdu perl-ack unzip python-pip gcc $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat&&pip config set global.index-url "https://pypi.mirrors.ustc.edu.cn/simple"&&pip install poetry ranger-fm||abort "依赖安装失败"
+pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl fish ncdu perl-ack unzip python-pip $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat&&pip config set global.index-url "https://pypi.mirrors.ustc.edu.cn/simple"&&pip install poetry ranger-fm||abort "依赖安装失败"
 
 Title="TRSS Yunzai Install Script $NAME ($VERSION)";BackTitle="作者：时雨🌌星空"
 menubox(){ MenuBox="$1";shift;dialog --title "$Title" --backtitle "$BackTitle $(date "+%F %T.%N")" --ok-button "确认" --cancel-button "取消" --menu "$MenuBox" 0 0 0 "$@" 3>&1 1>&2 2>&3;}
