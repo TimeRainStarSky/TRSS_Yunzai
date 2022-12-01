bb1caa31fd559085afec931fc424eafedcdd0c89
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-12-01 21:38:02 +0800

    细节优化

--- Install-MSYS2.sh
+++ Install-MSYS2.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai MSYS2 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202211260
+NAME=v1.0.0;VERSION=202212010
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -16,12 +16,13 @@ MSYS2ENV=mingw-w64-ucrt-x86_64
 type pacman &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到 pacman 命令，请确认安装了正确的 MSYS2 环境"
-pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl neofetch unzip fish ncdu perl-ack $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat||abort "依赖安装失败"
+pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux perl neofetch unzip fish ncdu perl-ack $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat ruby||abort "依赖安装失败"
 type fastfetch &>/dev/null||ln -vsf neofetch "$(dirname "$(command -v neofetch)")/fastfetch"
 [ -s /ucrt64/bin/fd ]&&{ mv -vf /ucrt64/bin/fd /ucrt64/bin/fdo;}
 [ -s /usr/bin/fd ]||echo -n '/ucrt64/bin/fdo "$@"|cygpath -mf -'>/usr/bin/fd
 [ -s /usr/bin/fzf ]||echo -n 'exec /ucrt64/bin/fzf "$@"'>/usr/bin/fzf
 [ -s /usr/bin/bat ]||echo -n 'exec /ucrt64/bin/bat "$@"'>/usr/bin/bat
+gem install lolcat
 
 Title="TRSS Yunzai Install Script $NAME ($VERSION)";BackTitle="作者：时雨🌌星空"
 menubox(){ MenuBox="$1";shift;dialog --title "$Title" --backtitle "$BackTitle $(date "+%F %T.%N")" --ok-button "确认" --cancel-button "取消" --menu "$MenuBox" 0 0 0 "$@" 3>&1 1>&2 2>&3;}
