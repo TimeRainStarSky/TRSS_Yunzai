9a4c964def1851f1146d890d6d6a2be1b1b62998
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-12-24 21:02:49 +0800

    细节优化

--- Install-Docker.sh
+++ Install-Docker.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai Docker 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202212230
+NAME=v1.0.0;VERSION=202212240
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B———————————————————————————
 $R TRSS$Y Yunzai$G Docker$C Script$O
@@ -124,14 +124,13 @@ RUN echo "zh_CN.UTF-8 UTF-8">/etc/locale.gen\
 RUN pacman -Syu --noconfirm --needed --overwrite "*" nodejs pnpm redis\
  && rm -rf /var/cache
 RUN echo -n '\''exec bash /root/TRSS_Yunzai/Main.sh "$@"'\''>/usr/local/bin/tsyz\
- && chmod 755 /usr/local/bin/tsyz
-EXPOSE 50831 54980'>Dockerfile
+ && chmod 755 /usr/local/bin/tsyz'>Dockerfile
 docker build -t trss:yunzai .||abort "Docker 容器构建失败"
 docker image prune -f
 echo "
 $Y- 正在启动 Docker 容器$O
 "
-docker run -itPd -h TRSS-Yunzai --name $DKNAME -v "$DIR":/root/TRSS_Yunzai --restart always trss:yunzai||abort "Docker 容器启动失败，若要重装容器，请先删除已安装容器，若要多开容器，请修改容器名"
+docker run -itd -h TRSS-Yunzai --name $DKNAME -v "$DIR":/root/TRSS_Yunzai --restart always $([ $DKNAME = TRSS_Yunzai ]&&echo "-p 50831:50831 -p 54980:54980"||echo "-p 50831 -p 54980") trss:yunzai||abort "Docker 容器启动失败，若要重装容器，请先删除已安装容器，若要多开容器，请修改容器名"
 mkdir -vp "$CMDPATH"&&echo -n "exec docker exec -it $DKNAME bash /root/TRSS_Yunzai/Main.sh "'"$@"'>"$CMDPATH/$CMD"&&chmod 755 "$CMDPATH/$CMD"||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：docker exec -it $DKNAME bash /root/TRSS_Yunzai/Main.sh"
 echo "
 $G- Docker 容器安装完成，启动命令：$CMD$O";exit;}
