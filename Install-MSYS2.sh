ea34dde8dd1d7f0ee35653df4fbf8e0bade33839
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-03-19 11:32:57 +0800

    细节优化

--- Install-MSYS2.sh
+++ Install-MSYS2.sh
@@ -1,5 +1,5 @@
 #TRSS Yunzai MSYS2 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202303180
+NAME=v1.0.0;VERSION=202303190
 R="[1;31m" G="[1;32m" Y="[1;33m" C="[1;36m" B="[1;m" O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y Yunzai$G Install$C Script$O
@@ -76,7 +76,7 @@ else
   mkpath /win/chromium
 fi||abort "安装失败";}
 
-type python &>/dev/null||{ GETVER="3.10.9"
+type python &>/dev/null||{ GETVER="3.10.10"
 echo "
 $Y- 正在安装 Python $GETVER$O
 "
